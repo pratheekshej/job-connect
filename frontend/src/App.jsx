adbb6b13107f00a3871b2c0e499cf08d34e22291
Pratheeksh Joseph <pratheekshej@gmail.com>
2024-09-29 22:05:29 -0400

    Initial frontend code commit to master.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,10 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Signup from "./components/auth/Signup";
 import Home from "./components/Home";
-import Login from "./components/auth/Login";
-import ResetPassword from "./components/auth/ResetPassword";
-import ChangePassword from "./components/auth/ChangePassword";
 
 const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/signup",
-    element: <Signup />,
-  },
-  {
-    path: "/reset-password",
-    element: <ResetPassword />,
-  },
-  {
-    path: "/change-password/:token",
-    element: <ChangePassword />,
   },
 ]);
 function App() {
--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -60,10 +60,10 @@ const Navbar = () => {
           </ul>
           {!user ? (
             <div className="flex items-center gap-2">
-              <Link to="/login">
+              <Link to="/">
                 <Button variant="outline">Login</Button>
               </Link>
-              <Link to="/signup">
+              <Link to="/">
                 <Button className="bg-[#6A38C2] hover:bg-[#5b30a6]">
                   Signup
                 </Button>
