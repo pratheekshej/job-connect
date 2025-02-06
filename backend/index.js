d990c02cf1cda35ebd923fd25000f7492b692b76
Pratheeksh Joseph <pratheekshej@gmail.com>
2025-02-05 21:37:40 -0500

    Updated package commands for running the app

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,11 @@ app.use(cookieParser());
 
 // CORS Configuration
 const corsOptions = {
-    origin: ['https://jobconnect-hag.netlify.app', 'http://localhost:5173'], // Replace with your frontend URL
+    origin: [
+        'https://jobconnect-hag.netlify.app',
+        'http://localhost:5173',
+        'http://localhost:5174',
+    ], // Replace with your frontend URL
     credentials: true, // Allow credentials (cookies, etc.)
 };
 
